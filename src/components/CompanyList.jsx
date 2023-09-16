9a0bc30ca126ba5b0b2e7f4da1372665dddb14d0
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-09-16 00:36:46 +0000

    add companylist component ✅

---
+++ src/components/CompanyList.jsx
@@ -0,0 +1,12 @@
+const CompanyList = () => {
+  return(
+    <>
+      <img src="./companys/CNN.svg" height="53" width="auto" alt="CNN" />
+      <img src="./companys/tirto.svg" height="53" width="auto" alt="tirto.id" />
+      <img src="./companys/forbes.svg" height="53" width="auto" alt="forbes" />
+      <img src="./companys/google.svg" height="53" width="auto" alt="google" />
+      <img src="./companys/newyorktimes.svg" height="53" width="auto" alt="the new york times" />
+    </>
+  )
+}
+export default CompanyList
