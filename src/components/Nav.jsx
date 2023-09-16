b0d057fe02ec4a0c80e0a629167a6693e8fe993a
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-09-16 06:58:01 +0000

    update nav ✅

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,10 +5,10 @@ const Nav = () => {
     <nav className="flex justify-between items-center">
       <div className="text-4xl">👋</div>
       <ul className="gap-x-16 text-dark hidden sm:flex">
-        <li><a href="#" >Beranda</a></li>
-        <li><a href="#">Fitur-fitur</a></li>
-        <li><a href="#">Teknologi</a></li>
-        <li><a href="#">FAQ</a></li>
+        <li><a href="#beranda" >Beranda</a></li>
+        <li><a href="#fitur">Fitur-fitur</a></li>
+        <li><a href="#teknologi">Teknologi</a></li>
+        <li><a href="#faq">FAQ</a></li>
       </ul>
       <Button size="small" color="primary">Download</Button>
     </nav>
